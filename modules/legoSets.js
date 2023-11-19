8b04d939c9a33eb9cb0607a4b0f92d309e7ba941
preet patel <preetpatelofficial004@gmail.com>
2023-11-18 21:51:19 -0500

    A4 changes

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,29 +1,68 @@
 /**************************************************************************************************
- * WEB322 – Assignment 3*
+ * WEB322 – Assignment 5*
  *
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
  * No part of this assignment has been copied manually or electronically from any other source
  * (including web sites) or distributed to other students.
  *
- * Name: Preet D. Patel     Student ID: 123845224      Date: 09/25/2023
+ * Name: Preet D. Patel     Student ID: 123845224      Date: 11/16/2023
  *
  **************************************************************************************************/
 
-const setData = require("../data/setData.json");
-const themeData = require("../data/themeData.json");
+require("dotenv").config();
+const Sequelize = require("sequelize");
 
-let sets = [];
+let sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
+  }
+);
+
+const Theme = sequelize.define(
+  "Theme",
+  {
+    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    name: Sequelize.STRING,
+  },
+  {
+    createdAt: false, // disable createdAt
+    updatedAt: false, // disable updatedAt
+  }
+);
+
+const Set = sequelize.define(
+  "Set",
+  {
+    set_num: { type: Sequelize.STRING, primaryKey: true },
+    name: Sequelize.STRING,
+    year: Sequelize.INTEGER,
+    num_parts: Sequelize.INTEGER,
+    theme_id: Sequelize.INTEGER,
+    img_url: Sequelize.STRING,
+  },
+  {
+    createdAt: false, // disable createdAt
+    updatedAt: false, // disable updatedAt
+  }
+);
+
+// create association
+Set.belongsTo(Theme, { foreignKey: "theme_id" });
+
+// let sets = [];
 
 const initialize = () => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     try {
-      setData.forEach((element) => {
-        element.theme = themeData.find(
-          (theme) => theme.id === element.theme_id
-        ).name;
-        sets.push(element);
-      });
-      resolve(sets);
+      await sequelize.sync().then(() => resolve());
     } catch (error) {
       reject("Failed to load Sets!");
     }
@@ -31,8 +70,9 @@ const initialize = () => {
 };
 
 const getAllSets = async () => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     try {
+      const sets = await Set.findAll({ include: [Theme] });
       resolve(sets);
     } catch (error) {
       reject("Faild to get Sets!");
@@ -40,13 +80,16 @@ const getAllSets = async () => {
   });
 };
 
-const getSetByNum = (setNum) => {
-  return new Promise((resolve, reject) => {
+const getSetByNum = async (setNum) => {
+  return new Promise(async (resolve, reject) => {
     try {
-      const set = sets.find((set) => set.set_num === setNum);
-      // console.log(set);
-      if (set) {
-        resolve(set);
+      const set = await Set.findAll({
+        where: { set_num: setNum },
+        include: [Theme],
+      });
+
+      if (set.length) {
+        resolve(set[0]);
       } else {
         reject("Unable to find requested set.");
       }
@@ -56,17 +99,17 @@ const getSetByNum = (setNum) => {
   });
 };
 
-const getSetsByTheme = (theme) => {
-  return new Promise((resolve, reject) => {
+const getSetsByTheme = async (theme) => {
+  return new Promise(async (resolve, reject) => {
     try {
       if (theme === "" || theme === " ") {
         reject(`Unable to fine requested set.`);
       } else {
-        const setsTheme = sets.filter((set) =>
-          set.theme.toLowerCase().includes(theme.toLowerCase())
-        );
+        const setsTheme = await Set.findAll({
+          include: [Theme],
+          where: { "$Theme.name$": { [Sequelize.Op.iLike]: `%${theme}%` } },
+        });
         if (setsTheme.length) {
-          // console.log(setsTheme.length)
           resolve(setsTheme);
         } else {
           reject("Unable to find requested set.");
@@ -78,6 +121,89 @@ const getSetsByTheme = (theme) => {
   });
 };
 
-module.exports = { initialize, getAllSets, getSetByNum, getSetsByTheme };
+const getAllThemes = async () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const themes = await Theme.findAll();
+      resolve(themes);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+const addSet = async (setData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await Set.create(setData);
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
+const editSet = async (set_num, setData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await Set.update(setData, { where: { set_num: set_num } });
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
+const deleteSet = async (set_num) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await Set.destroy({
+        where: {
+          set_num: set_num,
+        },
+      });
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+module.exports = {
+  initialize,
+  getAllSets,
+  getSetByNum,
+  getSetsByTheme,
+  getAllThemes,
+  addSet,
+  editSet,
+  deleteSet,
+};
+
+// Steps Done:
+
+// use neon tech to create new postgres DB
+// connect the db with pg admin
+// install sequelize
+// connect to db with sequelize object and new keyword
+// create models with define
+// connect models with foreign key
+// add data with bulkinsert if needed
+// sequelize
+//   .sync()
+//   .then(async () => {
+//     try {
+
+//       await Theme.bulkCreate(themeData);
+//       await Set.bulkCreate(setData);
+//       console.log("-----");
+//       console.log("data inserted successfully");
+//     } catch (err) {
+//       console.log("-----");
+//       console.log(err.message);
+//     }
+
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     console.log("Unable to connect to the database:", err);
+//   });
